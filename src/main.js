97909849a7f9cfae858d0af7790103ccfa0a89eb
Carlo Teo Pedretti <41264663+friendlynihilist@users.noreply.github.com>
2019-09-07 00:22:23 +0200

    updated media queries, created responsive navbar

--- src/main.js
+++ src/main.js
@@ -4,6 +4,44 @@ import {addSpecialArticle} from "./addArticle.js"
 import {articlesSidebarSelection} from "./articlesSelectionButtons"
 import {specialArticleSidebarSelection} from "./articlesSelectionButtons"
 
+
+//NAVBAR HAMBURGER
+(function () {
+
+    // Create mobile element
+    var mobile = document.createElement('div');
+    mobile.className = 'nav-mobile';
+    document.querySelector('.navbar').appendChild(mobile);
+
+    // hasClass
+    function hasClass(elem, className) {
+        return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    }
+
+    // toggleClass
+    function toggleClass(elem, className) {
+        var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+        if (hasClass(elem, className)) {
+            while (newClass.indexOf(' ' + className + ' ') >= 0) {
+                newClass = newClass.replace(' ' + className + ' ', ' ');
+            }
+            elem.className = newClass.replace(/^\s+|\s+$/g, '');
+        } else {
+            elem.className += ' ' + className;
+        }
+    }
+
+    // Mobile nav function
+    var mobileNav = document.querySelector('.nav-mobile');
+    var toggle = document.querySelector('.navbar__icons');
+    mobileNav.onclick = function () {
+        toggleClass(this, 'nav-mobile-open');
+        toggleClass(toggle, 'nav-active');
+    };
+})();
+
+
+
 // polyfill needed for using for loop on a dictionary
   /*
  * Object.prototype.forEach() polyfill
