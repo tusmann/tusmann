6927f97b5bfbb26ae140b11dd896038ade3b2a40
Carlo Teo Pedretti <41264663+friendlynihilist@users.noreply.github.com>
2019-09-13 21:35:47 +0200

    working Dungeon #2

--- src/styles.js
+++ src/styles.js
@@ -1,16 +1,12 @@
 import bauhaus from "./images/bauhaus.svg";
 import aldus_leaf from "./images/aldus_leaf.svg";
 import sakura from "./images/sakura.svg";
-<<<<<<< HEAD
 import die from "./images/die.svg";
 import manuzioLogic from "./manuzio";
 import dungeonLogic from "./dungeon";
-=======
-import manuzioLogic from "./manuzio"
-import rimpaLogic from "./rimpa"
+import rimpaLogic from "./rimpa";
 import twentyLogic from "./2020";
-import rollingDom from "./rollingDom"
->>>>>>> fb70e025dd78836fee2e0d9e5224274f3dc6f362
+import rollingDom from "./rollingDom";
 
 const styles = [
     {
