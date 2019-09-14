27bf19778ad20186899470df3cdff41e666d8c82
Francesca Mangialardo <mangiafrangette@gmail.com>
2019-09-14 12:47:35 +0200

    modifiche stili fra

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -1909,11 +1909,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-<<<<<<< HEAD
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1725" + '/');
-=======
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "50050" + '/');
->>>>>>> 6927f97b5bfbb26ae140b11dd896038ade3b2a40
 
   ws.onmessage = function (event) {
     checkedAssets = {};
