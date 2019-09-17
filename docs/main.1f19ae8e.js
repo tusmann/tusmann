bb6fc7730c62363bb22b318e14778853a443f496
Andrea Poltronieri <44606182+andreamust@users.noreply.github.com>
2019-09-17 19:05:00 +0200

    pre pull

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -2003,7 +2003,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63517" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64548" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
