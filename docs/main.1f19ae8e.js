5171a7a1bfe2a088081d25e1d4dd7ba0e47b4a91
Francesca Mangialardo <mangiafrangette@gmail.com>
2019-09-18 11:43:24 +0200

    ultime mod stili fra

--- docs/main.1f19ae8e.js
+++ docs/main.1f19ae8e.js
@@ -2004,7 +2004,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62161" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "32491" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
